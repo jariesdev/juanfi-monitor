141f001e5c99f08d026d5dc66909051770cfa36f
Jay Aries Flores <aries@sixtyswitched.com>
2024-02-15 02:11:08 +0800

    change sorting to log_time

--- log_repository.py
+++ log_repository.py
@@ -14,13 +14,13 @@ class LogRepository:
         cur = conn.cursor()
         if q is not None:
             cur.execute(
-                "SELECT * FROM juanfi_logs WHERE juanfi_logs.description LIKE ? ORDER BY id DESC",
+                "SELECT * FROM juanfi_logs WHERE juanfi_logs.description LIKE ? ORDER BY log_time DESC",
                 [
                     "%{}%".format(q),
                 ]
             )
         else:
-            cur.execute("SELECT * FROM juanfi_logs ORDER BY id DESC")
+            cur.execute("SELECT * FROM juanfi_logs ORDER BY log_time DESC")
 
         rows = cur.fetchall()
         conn.close()
