ac066de7fbab528f2afa8b14c772f6c02ea3077a
Jay Aries Flores <aries@sixtyswitched.com>
2024-02-14 23:07:35 +0800

    add allowed origin in CORS

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from typing import Union
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-
+from fastapi.middleware.cors import CORSMiddleware
 from juanfi_api import JuanfiApi
 from log_repository import LogRepository
 from user_repository import UserRepository
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
