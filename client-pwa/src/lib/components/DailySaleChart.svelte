f59e779541e846f4df11fb464ee5e0a5021fb9ef
Jay Aries Flores <aries@sixtyswitched.com>
2024-02-15 21:17:39 +0800

    add daily sales chart

---
+++ client-pwa/src/lib/components/DailySaleChart.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+    import Chart from 'chart.js/auto';
+    import {onMount} from "svelte";
+    import {apiUrl} from "$lib/store";
+
+    let chartData: any[] = []
+    let canvas: HTMLCanvasElement
+    let baseApiUrl: string = ''
+
+    interface iDailySale {
+        date: string
+        total: number
+    }
+
+    function renderChart(data: any[]): void {
+        const chart = new Chart(canvas, {
+            type: 'line',
+            data: data,
+            options: {
+                onClick: (e) => {
+                    const canvasPosition = getRelativePosition(e, chart);
+
+                    // Substitute the appropriate scale IDs
+                    const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
+                    const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
+                }
+            }
+        });
+    }
+
+    function loadChartData(): void {
+        const request = new Request(`${baseApiUrl}/daily-sales`, {method: "GET"})
+        fetch(request)
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    throw new Error("Something went wrong on API server!");
+                }
+            })
+            .then(({data}) => {
+            const labels = data.map((d: iDailySale) => d.date);
+            const saleData = data.map((d: iDailySale) => d.total);
+            chartData = {
+                labels: labels,
+                datasets: [{
+                    label: 'Daily Sales',
+                    data: saleData,
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                }]
+            };
+
+            renderChart(chartData)
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+            .finally(() => {
+                isLoading = false
+            })
+    }
+
+    apiUrl.subscribe(function (value) {
+        baseApiUrl = value
+    })
+
+    onMount(() => {
+        loadChartData()
+    })
+</script>
+
+
+<canvas bind:this={canvas}></canvas>
--- client-pwa/src/routes/+page.svelte
+++ client-pwa/src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import DailySaleChart from "$lib/components/DailySaleChart.svelte";
 </script>
 
 <svelte:head>
@@ -23,5 +24,6 @@
 		</p>
 	</h1>
 
+	<DailySaleChart></DailySaleChart>
 
 </section>
