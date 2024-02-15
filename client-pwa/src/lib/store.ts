c0c471fbe14b2abf0abbf2b8e562447e97079cda
Jay Aries Flores <aries@sixtyswitched.com>
2024-02-15 21:24:00 +0800

    change url

--- client-pwa/src/lib/store.ts
+++ client-pwa/src/lib/store.ts
@@ -1,4 +1,4 @@
 import { writable,readable } from 'svelte/store';
 
-export const apiUrl = readable('http://192.46.225.21:8001/');
+export const apiUrl = readable('http://192.46.225.21:8000/');
 export const count = writable(0);
