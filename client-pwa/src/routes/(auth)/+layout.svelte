e722783950882dbaeed51408ec6e4557c04ee57e
Jay Aries Flores <aries@sixtyswitched.com>
2024-03-05 04:34:50 +0800

    remove redundant import

--- client-pwa/src/routes/(auth)/+layout.svelte
+++ client-pwa/src/routes/(auth)/+layout.svelte
@@ -1,9 +1,4 @@
 <script>
-	import '../styles.css';
-	import '$lib/css/uikit.css'
-	import '$lib/js/uikit.min.js'
-	import '$lib/js/uikit-icons.min.js'
-	import OfflineAlert from "$lib/components/OfflineAlert.svelte";
 	import Header from "$lib/components/Header.svelte";
 </script>
 
