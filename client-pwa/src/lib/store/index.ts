958bfc95a1ca7dfab5daf88b8871a58d602554cd
Jay Aries Flores <aries@sixtyswitched.com>
2024-03-08 23:51:37 +0800

    fix api url

--- client-pwa/src/lib/store/index.ts
+++ client-pwa/src/lib/store/index.ts
@@ -1,6 +1,6 @@
 import {writable, readable, type Writable} from 'svelte/store';
 
-export const apiUrl = readable('http://127.0.0.1:8000');
+export const apiUrl = readable('/api');
 export const count = writable(0);
 
 export const currentUser: Writable<any> = writable(null)
