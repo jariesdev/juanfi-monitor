f603277a57110c3b227b84d70e7ad22a6b044bb5
Jay Aries Flores <jariesdev@gmail.com>
2025-11-15 11:57:22 +0800

    fix user login form

--- client-pwa/src/routes/(auth)/+layout.server.ts
+++ client-pwa/src/routes/(auth)/+layout.server.ts
@@ -5,6 +5,7 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({cookies}) => {
     const token = cookies.get('auth_token')
     const expiry = cookies.get('auth_token_expiry')
+
     if (!(token && expiry)) {
         throw redirect(302, '/login')
     }
