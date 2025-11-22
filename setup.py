0272d310a6cbf5c13cc43b643c3a26c86c59d553
Jay Aries Flores <jariesdev@gmail.com>
2025-11-22 08:47:29 +0800

    Use poetry and docker

---
+++ setup.py
@@ -0,0 +1,10 @@
+# Source - https://stackoverflow.com/a
+# Posted by pce, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-11-21, License - CC BY-SA 4.0
+
+#!/usr/bin/env python
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
