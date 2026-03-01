2896b9bf3f06663c7295f1e31b066f2df6676f7e
Jay Aries Flores <jariesdev@gmail.com>
2026-03-01 16:18:35 +0800

    added monthly sales chart

--- client-pwa/src/routes/(auth)/home/+page.svelte
+++ client-pwa/src/routes/(auth)/home/+page.svelte
@@ -6,6 +6,7 @@
 	import DailySaleChart from '$lib/components/DailySaleChart.svelte';
 	import VendoMonthlyUserChart from '$lib/components/VendoMonthlyUserChart.svelte';
 	import VendoCapacityChart from '$lib/components/VendoCapacityChart.svelte';
+	import MonthlySaleChart from '$lib/components/MonthlySaleChart.svelte';
 	import {onMount} from "svelte";
 
 	onMount(() => {
@@ -16,6 +17,8 @@
 <section class="uk-section">
 	<h3 class="uk-text-light uk-text-center">Daily Sales</h3>
 	<DailySaleChart />
+	<h3 class="uk-text-light uk-text-center">Monthly Sales</h3>
+	<MonthlySaleChart />
 	<h3 class="uk-text-light uk-text-center">Vendo Capacity Counter</h3>
 	<VendoCapacityChart />
 	<h3 class="uk-text-light uk-text-center">Active User History</h3>
