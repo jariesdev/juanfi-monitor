1f60308c57c7352b6a9a3a8c484fd173ec8bd11c
Jay Aries Flores <jariesdev@gmail.com>
2026-05-02 13:37:11 +0800

    remove unused function getCellValue

--- client-pwa/src/lib/components/DataTable.svelte
+++ client-pwa/src/lib/components/DataTable.svelte
@@ -107,10 +107,6 @@
 		loadData();
 	}
 
-	function getCellValue(item: RowItem, header: TableHeader) {
-		return get(item, header.field, '');
-	}
-
 	// reset when search
 	const resetTableQuery = () => {
 		currentPage = 1;
