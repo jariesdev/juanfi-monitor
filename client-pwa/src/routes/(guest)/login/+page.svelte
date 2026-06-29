7352813fa53d1020f24a64c92f997d24c5ec6d3f
Jay Aries Flores <jariesdev@gmail.com>
2026-06-29 12:30:17 +0800

    Update brand name from "VendoReport" to "Vendo Reports" on login page

--- client-pwa/src/routes/(guest)/login/+page.svelte
+++ client-pwa/src/routes/(guest)/login/+page.svelte
@@ -9,7 +9,7 @@
 <div class="login-wrap">
 	<div class="card">
 		<div class="card-header">
-			<div class="brand">VendoReport</div>
+			<div class="brand">Vendo Reports</div>
 			<p class="subtitle">Sign in to your account</p>
 		</div>
 		<div class="card-body">
